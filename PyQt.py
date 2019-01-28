9f21d96b4b0ec71a752826efc43d7aabfb63fcbc
Sviatoslav <sviatoslav.pereverziev@gmail.com>
2019-01-28 07:01:32 +0200

    Wrote a new server using the module threading. PyQt application similar to what should be but still need to add functionality.

--- PyQt.py
+++ PyQt.py
@@ -1,53 +1,142 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
-from configparser import ConfigParser
+# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets
 import socket
-import pickle
+
+connects = {}
 
 
-class Example(QMainWindow):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(800, 600)
 
-    def __init__(self):
-        super().__init__()
+        self.host_text = QtWidgets.QTextEdit(Form)
+        self.host_text.setGeometry(QtCore.QRect(330, 90, 161, 21))
+        self.host_text.setObjectName("textEdit")
 
-        self.initUI()
+        self.clients_text = QtWidgets.QTextEdit(Form)
+        self.clients_text.setGeometry(QtCore.QRect(40, 30, 261, 191))
+        self.clients_text.setObjectName("clients_text")
 
-    def initUI(self):
-        btn1 = QPushButton("Connect", self)
-        btn1.move(30, 50)
+        self.port_text = QtWidgets.QTextEdit(Form)
+        self.port_text.setGeometry(QtCore.QRect(510, 90, 151, 21))
+        self.port_text.setObjectName("port_text")
 
-        btn2 = QPushButton("Send", self)
-        btn2.move(150, 50)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(670, 90, 61, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.add)
 
-        btn1.clicked.connect(self.connect)
-        btn2.clicked.connect(self.send)
+        self.massege_text = QtWidgets.QTextEdit(Form)
+        self.massege_text.setGeometry(QtCore.QRect(270, 260, 391, 21))
+        self.massege_text.setObjectName("massege_text")
 
-        self.statusBar()
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(670, 260, 61, 31))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.send)
 
-        self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('Connect&Send')
-        self.show()
+        self.log_text = QtWidgets.QTextEdit(Form)
+        self.log_text.setGeometry(QtCore.QRect(40, 320, 691, 221))
+        self.log_text.setObjectName("log_text")
 
-    def connect(self):
-        cfg = ConfigParser()
-        cfg.read('settings.ini')
-        HOST = cfg.get('server', 'host')
-        PORT = int(cfg.get('server', 'port'))
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(40, 10, 60, 16))
+        self.label.setObjectName("label")
 
-        self.sock = socket.socket()
-        self.sock.connect((HOST, PORT))
-        self.statusBar().showMessage('Connect')
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(330, 60, 60, 16))
+        self.label_2.setObjectName("label_2")
+
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(510, 60, 60, 16))
+        self.label_3.setObjectName("label_3")
+
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(330, 10, 91, 16))
+        self.label_4.setObjectName("label_4")
+
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(40, 240, 60, 16))
+        self.label_5.setObjectName("label_5")
+
+        self.label_6 = QtWidgets.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(150, 240, 91, 16))
+        self.label_6.setObjectName("label_6")
+
+        self.label_7 = QtWidgets.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(270, 240, 60, 16))
+        self.label_7.setObjectName("label_7")
+
+        self.label_8 = QtWidgets.QLabel(Form)
+        self.label_8.setGeometry(QtCore.QRect(40, 300, 60, 16))
+        self.label_8.setObjectName("label_8")
+
+        self.comboBox = QtWidgets.QComboBox(Form)
+        self.comboBox.setGeometry(QtCore.QRect(40, 260, 104, 26))
+        self.comboBox.setObjectName("comboBox")
+
+        self.comboBox_2 = QtWidgets.QComboBox(Form)
+        self.comboBox_2.setGeometry(QtCore.QRect(150, 260, 104, 26))
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.comboBox_2.addItems(["ping", "status",
+                                  "lower", "quit"])
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Add"))
+        self.pushButton_2.setText(_translate("Form", "Send"))
+        self.label.setText(_translate("Form", "Clients:"))
+        self.label_2.setText(_translate("Form", "Host:"))
+        self.label_3.setText(_translate("Form", "Port:"))
+        self.label_4.setText(_translate("Form", "New client:"))
+        self.label_5.setText(_translate("Form", "Client:"))
+        self.label_6.setText(_translate("Form", "Request type:"))
+        self.label_7.setText(_translate("Form", "Massege:"))
+        self.label_8.setText(_translate("Form", "Log:"))
+
+    def add(self):
+        host = self.host_text.toPlainText()
+        if not host:
+            host = 'localhost'
+        try:
+            port = int(self.port_text.toPlainText())
+            connects[str((host, port))] = socket.socket()
+            try:
+                connects[str((host, port))].connect((host, port))
+                self.comboBox.addItem(str((host, port)))
+                print(connects)
+                self.clients_text.setText(f'Подключен:\n Host:{host} Port:{port}\n')
+            except socket.gaierror:
+                self.log_text.setText('Неизвесное host имя\n')
+            except ConnectionRefusedError:
+                self.log_text.setText('В соединении отказано\n')
+
+        except ValueError:
+            self.log_text.setText('Неправильное значение порта\n')
 
     def send(self):
-        my_dict = {"id": 2, "name": "abc"}
-        try:
-            self.sock.send(pickle.dumps(my_dict))
-            self.statusBar().showMessage('Send')
-        except AttributeError:
-            self.statusBar().showMessage('First click on connect')
+        client = self.comboBox.currentText()
+        request_type = self.comboBox_2.currentText()
+        massage = self.massege_text.toPlainText()
+        connects[client].send(bytes(f'{request_type}:{massage}\n', encoding='UTF-8'))
+        data = connects[self.comboBox.currentText()].recv(1024)
+        data = data.decode("utf-8")
+        data = data.replace('\n', '').replace('\r', '')
+        self.log_text.setText(data)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
     sys.exit(app.exec_())
+
--- server.py
+++ server.py
@@ -1,35 +1,75 @@
 import socket
-import pickle
 from configparser import ConfigParser
+from _thread import *
+from time import time
+import sys
 
-cfg = ConfigParser()
-cfg.read('settings.ini')
-HOST = cfg.get('server', 'host')
-PORT = int(cfg.get('server', 'port'))
-LISTENERS = int(cfg.get('server', 'listeners'))
 clients = []
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(LISTENERS)
-conn, addr = sock.accept()
+
+def get_settings():
+    cfg = ConfigParser()
+    cfg.read('settings.ini')
+    host = cfg.get('server', 'host')
+    listeners = int(cfg.get('server', 'listeners'))
+
+    if len(sys.argv) > 1:
+        port = (int(sys.argv[1]))
+    else:
+        port = int(cfg.get('server', 'port'))
+
+    return {'host': host, 'port': port, 'listeners': listeners}
 
 
-quit = False
-print('connected:', addr)
+def new_sock():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((list_cfg['host'], list_cfg['port']))
+    sock.listen(list_cfg['listeners'])
+    return sock
 
-while not quit:
-    try:
+
+def client(conn):
+    conn.send(bytes('Hello from server!\n', encoding='UTF-8'))
+    while True:
         data = conn.recv(1024)
-        data = pickle.loads(data)
+        data = data.decode("utf-8")
+        data = data.replace('\n', '').replace('\r', '')
+        data = data.split(':')
+        try:
+            if data[0] == 'ping' and data[1] == '*':
+                conn.send(bytes('ping:+\n', encoding='UTF-8'))
+
+            elif data[0] == 'status':
+                reply = f'status:(time:{int(time())}, connections_count:{len(clients)}, ' \
+                        f'available quantity: {list_cfg["listeners"]})\n'
+                conn.send(bytes(reply, encoding='UTF-8'))
+
+            elif data[0] == 'lower':
+                lower_str = str(data[1]).lower()
+                conn.send(bytes(f'lower:{lower_str}\n', encoding='UTF-8'))
+
+            elif data[0] == 'quit':
+                print(conn.getpeername())
+                clients.remove(conn.getpeername())
+                conn.send(bytes('quit\n', encoding='UTF-8'))
+                break
+            else:
+                conn.send(bytes('not recognized\n', encoding='UTF-8'))
+        except IndexError:
+            conn.send(bytes('not recognized\n', encoding='UTF-8'))
+
+        if not data:
+            break
+    conn.close()
+
+
+if __name__ == '__main__':
+    list_cfg = get_settings()
+    my_sock = new_sock()
+
+    while True:
+        conn, addr = my_sock.accept()
+        print('connected:', addr)
         if addr not in clients:
             clients.append(addr)
-
-    except EOFError:
-        print('')
-
-    print(data)
-
-
-
-conn.close()
+        start_new_thread(client, (conn,))
